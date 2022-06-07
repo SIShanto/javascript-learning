ecfe3932e438afa8f1513c77a6b080af23f88162
SIShanto <96788338+SIShanto@users.noreply.github.com>
2022-06-07 23:30:49 +0600

    Update file1.js

--- file1.js
+++ file1.js
@@ -183,7 +183,7 @@ else
 }
 console.log(drink);
 
-// ternary operator/ conditional opertor
+           // ternary operator/ conditional opertor
 
 age = 7;
 drink = age<=4 ? "milk" : "coffee";
@@ -443,6 +443,8 @@ let oneMoreArray=["jjjfd","lll"]
 // array2 =[...array1 ,"ishli","egfhoieg"];
 array2 =[...array1,...oneMoreArray];
 
+// ... is spread oparator
+
 array1.push("item1");
 console.log(array1===array2);
 console.log(array1);
@@ -456,6 +458,11 @@ fruit=["amm","jaam","lichu","kola"];
 // console.log(fruit[fruit.length-3]);
 // console.log(fruit[fruit.length-2]);
 // console.log(fruit[fruit.length-1]);
+for(i=0;i<fruit.length;i++)
+{
+    console.log(fruit[i]);
+}
+
 let fruit1=[];
 for(i=0;i<fruit.length;i++)
 {
@@ -472,3 +479,239 @@ for(let g of animal)
 {
     console.log(g);
 }
+
+// count=0;
+// while(count<10)
+// {
+//     console.log(count);
+//     count++;
+// }
+                  //  arrray destruction
+
+const myArray=["value1","value2","value3","value4"]
+
+// let [newArray1,newArray2,newArray3]=myArray;
+// let newArray1=myArray.slice(0,3);
+let [newArray1,newArray2,...newArray3]= myArray
+
+console.log(newArray3); 
+
+//  objects
+// arrays are good but not sufficient
+//  for real world data
+// objects store key value pairs
+// objects don't have index
+
+// how to creat objects
+// const person={name:"shanto",age:23,};
+const person={name:"shanto",age:23,hobbies:["codding","driving","travelling",
+"eating"]};
+console.log(person);
+console.log(typeof person);
+// how to access data from objects
+// console.log(person.name);
+// console.log(person.age);
+console.log(person.hobbies);  //This line has made new array. So we can do anything with this array
+person.hobbies.push("resting");
+let [ , ,...myhobbies]=person.hobbies;
+console.log(myhobbies);
+
+
+
+    //** how to add key value pair to objects **//
+
+// person.gender="Male";
+// person.jojo=["keke","koko"];
+
+
+        
+       // difference between square bracket and dot
+
+
+const key="email";//we need to add this in person object
+// person.key="kskn@gmail.com"; 
+// person["key"]="kskn@gmail.com";//here we need value  'email' in person object, not the value key. so we do it as below
+
+person[key]="kskn@gmail.com"; // here we get the value of key is email, not key as upper line
+console.log(person);
+
+            //*** how to iterate object //****
+
+// for in loop
+// object.keys
+
+for (let key in person)
+{
+    //  console.log(person[key]); //here we get the values  of the properties in person
+    console.log(`${key}:${person[key]}`); //here we get the values with the properties.Template string should be used
+    
+}
+
+// Object.keys method
+// here we have to use for of loop
+
+console.log(Object.keys(person)); //Object.keys here Object O is always capital
+
+ for(let key of Object.keys(person))
+ {
+     console.log(`${key}:${person[key]}`);//template string should be used
+
+ }
+
+
+               //computed properties
+
+const key1="objkey1";
+const key2="objkey2";
+
+const valu1="myvalue1";
+const valu2="myvalue2";
+
+const obje={
+    [key1]: valu1,
+    [key2] :valu2
+}
+console.log(obje);
+
+// const obje={}; 
+
+// obje[key1]=valu1;
+// obje[key2]=valu2;
+
+// console.log(obje);
+
+
+
+              // spread operator in object case
+
+const obj1={
+    key3:"val1", //Always remember, key will be one type in every programming .there will be no more key1. if key1 is used in another line, the new line will use key2. not same key1
+
+    key4:"val2", //Always remember, key will be one type in every programming .there will be no more key1. if key1 is used in another line, the new line will use key2. not same key1
+
+}
+const obj2={
+    key5:"val5", //Always remember, key will be one type in every programming .there will be no more key1. if key1 is used in another line, the new line will use key2. not same key1
+
+    key6:"val6", //Always remember, key will be one type in every programming .there will be no more key1. if key1 is used in another line, the new line will use key2. not same key1
+
+}
+
+// const newObj={...obj1,...obj2, key7:"val7"};
+//  const newObj={..."abcd"}; //it will spread with index number
+const newObj={...["val1","val2","val3"]}; //it will spread with index number with array
+console.log(newObj);
+
+
+// object destruction
+
+const car={
+    name:"toyta",
+    power: "106hp",
+    color:"Silver"
+}
+const spark="fuel";
+car[spark]="Octane";
+
+// const {name,...power}=car; //variable name is constructed as key name by default. 
+const {name:var1, ...var2}=car; //variable name is constructed as key name by default. but if i want to change the variables then this should be done. remember, clonning variable for rest properties can't change name. like ...power:var2 is not possible.      but ...var2 is possible.
+
+console.log(var2);
+                
+
+               // objects inside array
+         // very useful in real world applicaton
+
+const users=[
+    {userId:1, firstNam:"Shanto",gender:"Male"},
+    {userId:2, firstNam:"Shanto",gender:"Male"},
+    {userId:3, firstNam:"sokina",gender:"feMale"},
+]
+// console.log(users);
+
+for(let use=0; use<users.length;use++)
+{
+    console.log(users[use]);
+}
+
+for(let use of users)
+{
+    console.log(use);
+}
+
+// we can use both types of for loop above
+
+              // nested destructuring
+
+// const [user1, user2, user3]=users; //destructuring from array
+const [{firstNam}, {userId} ,{gender}]=users; //destructuring from objects. here Name will be destructured from frist object and gender will be destructured from third object.
+console.log (firstNam,userId,gender,);
+
+
+
+
+              // *******FUNCTION****** //
+
+ 
+// creat a function
+
+function birthday()
+{
+    console.log("goo to sleeep");
+}
+
+//call that function
+birthday();
+
+function sumTwoNumbers(numb1,numb2)  // In created function numb1 and numb2 are called perameters
+{
+    return numb1+numb2;
+}
+
+let Total=sumTwoNumbers(9,5); // In called function 9 and 5 are called arguments
+
+// When argument values are put in function, they are stored in perameters and goes to return.///////
+
+console.log(Total);
+
+// even or odd number finder
+
+function isOdd(numb)
+{
+    if( numb%2 !==0)
+    {
+        return console.log(`The number is ${numb} and it is Odd`);
+    }
+    return console.log(`The number is ${numb} and it is Even`);
+}
+isOdd(3);
+
+//  function
+// input string
+// output firstCharacter
+
+function fChar(string)
+{
+   return string[0];
+}
+console.log(fChar("JAPAn"));
+
+ 
+
+
+function findIndex(array,target)
+ {
+     for(let i=0;i<array.length;i++) // "i" is the value of index number. and array[i] is the value of content in that "i"th index number.
+     {
+          if(array[i]===target)
+          {
+              return i;
+          }
+     }
+     return -1;
+ }
+
+ const arr=[1,3,5,8,6];
+ const ans=findIndex(arr,5);
+ console.log(ans);
+
